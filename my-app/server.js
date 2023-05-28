7d42480d0cd1d9db8f7fb4577c21d0fa31dac6dd
marek <marek.spisiak165@gmail.com>
2023-05-28 17:25:13 +0000

    idkTest

---
+++ my-app/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
